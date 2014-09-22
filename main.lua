674c3c89dc527e2312801c91df5b6a5dc31be1ce
tiagowz <tiagowz@icloud.com>
2014-09-22 18:27:02 -0300

    versao_3.1
    adicionando mais objetos

--- main.lua
+++ main.lua
@@ -23,10 +23,12 @@ local backgroundCeu = display.newImage("Imagens/backgroundCeu.png",width,height)
 local backgroundGrama = display.newImage( "Imagens/backgroundGrama.png", width, 260)
 local backgroundChao = display.newImage("Imagens/backgroundChao.png", width, 315)
 local arvore = display.newImage( "Imagens/arvoreP.png", 480, 180)
-
-
---arvore:translate( width , height )
-
+local pedra = display.newImage( "Imagens/pedra.png", 200, 280)
+pedra.xScale= 0.4; pedra.yScale = 0.4
+local nuvem1 = display.newImage( "Imagens/nuvemP.png", 200, 80)
+nuvem1.xScale= 0.4; nuvem1.yScale = 0.4
+local nuvem2 = display.newImage( "Imagens/nuvemP.png", 200, 50)
+nuvem1.xScale= 0.5; nuvem1.yScale = 0.5
 
 --Sprite do Ponei
 local sheet1 = graphics.newImageSheet( "Imagens/ponySpriteM.png", { width=128, height=104, numFrames=8 } )
@@ -44,10 +46,11 @@ physics.addBody(instance1)
 --função trouch para salto do ponnei
 local function onScreenTouch( event )
 		if event.phase == "began" then
-				instance1:applyForce(0, 50, instance1.x, instance1.y)
+				instance1:applyForce(0, 65, instance1.x, instance1.y)
 				end
 	end
 
+--movimento dos objetos
 local tPrevious = system.getTimer()
 
 local function mover( event )
@@ -55,15 +58,28 @@ local function mover( event )
 	local tDelta = event.time - tPrevious
 	tPrevious = event.time
 
-	local xOffset = ( 0.1 * tDelta )
+	local xOffset = ( 0.04 * tDelta )
+	local xOffset2 = ( 0.1 * tDelta )
+	local xOffsetNuvem1 = ( 0.008 * tDelta )
+	local xOffsetNuvem2 = ( 0.004 * tDelta )
 	arvore.x = arvore.x - xOffset
+	pedra.x = pedra.x - xOffset2
+	nuvem1.x = nuvem1.x - xOffsetNuvem1
+	nuvem2.x = nuvem2.x - xOffsetNuvem2
 
 	if (arvore.x + arvore.contentWidth) < 0 then
 		arvore:translate( 480 * 2, 0)
 	end
-	-- body
+	if (pedra.x + pedra.contentWidth) < 0 then
+		pedra:translate( 360 * 2, 0)
+	end
+	if (nuvem1.x + nuvem1.contentWidth) < 0 then
+		nuvem1:translate( 360 * 2, 0)
+	end
+	if (nuvem2.x + nuvem2.contentWidth) < 0 then
+		nuvem2:translate( 360 * 2, 0)
+	end
 end
-
 
 --acionamento do evento "touch"
 Runtime:addEventListener("touch", onScreenTouch)
